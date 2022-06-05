cdf030038ed35430f64682ece741f22d38f3633f
robdll <dilillo.roberto@gmail.com>
2022-06-05 19:05:59 +0100

    removes log

--- server/middleware/bodyValidator.js
+++ server/middleware/bodyValidator.js
@@ -5,12 +5,14 @@ function bodyValidator(req, res, next) {
     }
     var routeName = req.url.split('/')[1];
     const requiredProps = routeProperties[`${routeName}_${method}`];
+
     if(!requiredProps ) {
         return next();
     }
     const hasAllProps = requiredProps
         .map( prop => !!req.body[prop])
         .reduce( (a,b) => a && b, true);
+
     if(hasAllProps) {
         return next();
     } 
@@ -19,7 +21,6 @@ function bodyValidator(req, res, next) {
         code: 400,
         detail: `Following props are missing: ${missingProps}`
     }
-    console.log('err', err)
     return next(err);
 }
 
